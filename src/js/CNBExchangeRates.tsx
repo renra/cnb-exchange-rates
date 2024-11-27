d8861f5ee37bbdaff5bf355a9a4cb22707789273
Jan Renra Gloser <jan.renra.gloser@gmail.com>
2024-11-27 16:33:48 +0100

    Stuff

--- src/js/CNBExchangeRates.tsx
+++ src/js/CNBExchangeRates.tsx
@@ -150,7 +150,7 @@ const InnerInner = (props: InnerInnerProps) : JSX.Element => {
 
   React.useEffect(
     () => {
-      props.walletManager.onMounted().catch((err) => alert(JSON.stringify(err)))
+      props.walletManager.onMounted()
 
       return () => {
         props.walletManager.onUnmounted()
--- src/js/polyfills.ts
+++ src/js/polyfills.ts
@@ -1,14 +1,9 @@
 window.global = window.globalThis;
 window.process = {
-  // @ts-ignore TS2353
   browser: true,
   version: "0",
   env: {}
 };
 
 import buffer from 'buffer'
-// // @ts-ignore TS7016
-// import cryptoBrowserify from 'crypto-browserify'
-
 window.Buffer = buffer.Buffer
-// crypto = cryptoBrowserify
